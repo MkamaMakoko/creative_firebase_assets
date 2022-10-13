33e4b761e78bcf719b5c8c3ef68a6930295bdf00
Anthony Mkama <mkama.afm@gmail.com>
2022-10-13 08:19:00 +0300

    upgraded file_picker package

--- lib/creative_firebase_assets.dart
+++ lib/creative_firebase_assets.dart
@@ -1,7 +1,5 @@
 library creative_firebase_assets;
 
-import 'package:creative_firebase_assets/file_asset/file_asset.dart';
-export 'package:firebase_storage/firebase_storage.dart';
-
-
-
+export 'package:creative_firebase_assets/providers/file_picker/provider.dart';
+export 'package:creative_firebase_assets/file_asset/file_asset.dart';
+export 'package:creative_firebase_assets/providers/file_assets.dart';
